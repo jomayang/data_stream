ddf7b771cb6b752181cd8385385c31097727d780
richard <hannemanrichard7@gmail.com>
2023-04-12 11:32:55 +0100

    second

--- index.js
+++ index.js
@@ -8,92 +8,96 @@ const supabaseKey =
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-cron.schedule(
-  "1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52,55,58 * * * *",
-  async () => {
-    console.log("running a task on schedule");
-    try {
-      // Retrieve parcels payment_status=not-ready&
-      const parcelsResponse = await axios.get(
-        `https://api.yalidine.app/v1/parcels/?order_by=date_last_status&page_size=100`,
-        {
-          headers: {
-            "X-API-ID": "92129974643421801058",
-            "X-API-TOKEN":
-              "JyWPYR7SpCZlWMSO4rQKcqrPAzNwmftAMdv49z07EVtwlTU3aBVNaFF2GLes2uoH",
-          },
-        }
-      );
-      const parcels = parcelsResponse.data.data.map((parcel) => ({
-        tracking: parcel.tracking,
-        first_name: parcel.firstname,
-        last_name: parcel.familyname,
-        created_at: new Date(parcel.date_creation),
-        date_last_status: new Date(parcel.date_last_status),
-        phone: parcel.contact_phone,
-        wilaya: parcel.to_wilaya_name,
-        commune: parcel.to_commune_name,
-        center: parcel.current_center_name,
-        product: parcel.product_list,
-        last_status: parcel.last_status,
-        address: parcel.address,
-        payment_status: parcel.payment_status,
-        tracker_id: +parcel.order_id.replace("order_", ""),
-        is_stopdesk: parcel.is_stopdesk,
-      }));
-
-      // Extract tracking numbers
-      const trackings = parcels.map((parcel) => parcel.tracking);
-      const trackingsStr = trackings.join(",");
-
-      const historiesResponse = await axios.get(
-        `https://api.yalidine.app/v1/histories/?tracking=${trackingsStr}&page_size=900`,
-        {
-          headers: {
-            "X-API-ID": "92129974643421801058",
-            "X-API-TOKEN":
-              "JyWPYR7SpCZlWMSO4rQKcqrPAzNwmftAMdv49z07EVtwlTU3aBVNaFF2GLes2uoH",
-          },
-        }
-      );
-
-      const histories = historiesResponse.data.data.map((history) => ({
-        tracking: history.tracking,
-        created_at: new Date(history.date_status),
-        status: history.status,
-        reason: history.reason,
-        center_name: history.center_name,
-        wilaya_name: history.wilaya_name,
-        commune_name: history.commune_name,
-      }));
-
-      const { data, error } = await supabase
-        .from("parcels")
-        .upsert(parcels)
-        .select();
-
-      if (data) {
-        console.log("success");
+// cron.schedule(
+//   "1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52,55,58 * * * *",
+//   async () => {
+const start = async () => {
+  console.log("running a task on schedule");
+  try {
+    // Retrieve parcels payment_status=not-ready&
+    const parcelsResponse = await axios.get(
+      `https://api.yalidine.app/v1/parcels/?order_by=date_last_status&page_size=100`,
+      {
+        headers: {
+          "X-API-ID": "92129974643421801058",
+          "X-API-TOKEN":
+            "JyWPYR7SpCZlWMSO4rQKcqrPAzNwmftAMdv49z07EVtwlTU3aBVNaFF2GLes2uoH",
+        },
       }
+    );
+    const parcels = parcelsResponse.data.data.map((parcel) => ({
+      tracking: parcel.tracking,
+      first_name: parcel.firstname,
+      last_name: parcel.familyname,
+      created_at: new Date(parcel.date_creation),
+      date_last_status: new Date(parcel.date_last_status),
+      phone: parcel.contact_phone,
+      wilaya: parcel.to_wilaya_name,
+      commune: parcel.to_commune_name,
+      center: parcel.current_center_name,
+      product: parcel.product_list,
+      last_status: parcel.last_status,
+      address: parcel.address,
+      payment_status: parcel.payment_status,
+      tracker_id: +parcel.order_id.replace("order_", ""),
+      is_stopdesk: parcel.is_stopdesk,
+    }));
 
-      if (error) {
-        console.log(error);
+    // Extract tracking numbers
+    const trackings = parcels.map((parcel) => parcel.tracking);
+    const trackingsStr = trackings.join(",");
+
+    const historiesResponse = await axios.get(
+      `https://api.yalidine.app/v1/histories/?tracking=${trackingsStr}&page_size=900`,
+      {
+        headers: {
+          "X-API-ID": "92129974643421801058",
+          "X-API-TOKEN":
+            "JyWPYR7SpCZlWMSO4rQKcqrPAzNwmftAMdv49z07EVtwlTU3aBVNaFF2GLes2uoH",
+        },
       }
+    );
 
-      const { data: dataHistories, error: errorHistories } = await supabase
-        .from("histories")
-        .upsert(histories)
-        .select();
+    const histories = historiesResponse.data.data.map((history) => ({
+      tracking: history.tracking,
+      created_at: new Date(history.date_status),
+      status: history.status,
+      reason: history.reason,
+      center_name: history.center_name,
+      wilaya_name: history.wilaya_name,
+      commune_name: history.commune_name,
+    }));
 
-      if (dataHistories) {
-        console.log("histories upserted");
-      }
+    const { data, error } = await supabase
+      .from("parcels")
+      .upsert(parcels)
+      .select();
 
-      if (errorHistories) {
-        console.log(errorHistories);
-      }
-    } catch (error) {
+    if (data) {
+      console.log("success");
+    }
+
+    if (error) {
       console.log(error);
     }
+
+    const { data: dataHistories, error: errorHistories } = await supabase
+      .from("histories")
+      .upsert(histories)
+      .select();
+
+    if (dataHistories) {
+      console.log("histories upserted");
+    }
+
+    if (errorHistories) {
+      console.log(errorHistories);
+    }
+  } catch (error) {
+    console.log(error);
   }
-);
+};
+
+start();
+// }
+// );
