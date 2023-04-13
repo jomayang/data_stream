e9443aec1240c15a12c18cad336e530f25201b12
Hanneman richard <84267783+jomayang@users.noreply.github.com>
2023-04-13 14:11:31 +0100

    fix track issue

--- index.js
+++ index.js
@@ -47,14 +47,8 @@ const start = async () => {
       tracking: parcel.tracking,
       first_name: parcel.firstname,
       last_name: parcel.familyname,
-      created_at: changeTimeZone(
-        new Date(parcel.date_creation),
-        "Europe/London"
-      ),
-      date_last_status: changeTimeZone(
-        new Date(parcel.date_last_status),
-        "Europe/London"
-      ),
+      created_at: new Date(parcel.date_creation),
+      date_last_status: new Date(parcel.date_last_status),
       phone: parcel.contact_phone,
       wilaya: parcel.to_wilaya_name,
       commune: parcel.to_commune_name,
@@ -86,10 +80,7 @@ const start = async () => {
     // console.log(historiesResponse.data.data);
     const histories = historiesResponse.data.data.map((history) => ({
       tracking: history.tracking,
-      created_at: changeTimeZone(
-        new Date(history.date_status),
-        "Europe/London"
-      ),
+      created_at: history.date_status,
       status: history.status,
       reason: history.reason,
       center_name: history.center_name,
