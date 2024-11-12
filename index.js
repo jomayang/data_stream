b892fa589901bbdb82c69dcd63aaaf6c7acb62d2
Hanneman richard <84267783+jomayang@users.noreply.github.com>
2024-11-12 13:14:01 +0100

    fix tracker id

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const start = async () => {
       last_status: parcel.last_status,
       address: parcel.address,
       payment_status: parcel.payment_status,
-      tracker_id: 28 /* +parcel.order_id.replace("order_", "") */,
+      tracker_id: +parcel.order_id.replace("order_", ""),
       is_stopdesk: !!parcel.stopdesk_id,
       price: parcel.price,
       delivery_fee: parcel.delivery_fee,
